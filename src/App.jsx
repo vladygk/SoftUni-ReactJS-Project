dded9246036af5b67cf470ae2fad0d3cfb6c4de8
Vladislav Kotsev <vladygk8@gmaila.com>
2023-04-09 23:39:32 +0300

    Add favorite collection

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { LoginPage } from "./components/LoginPage/LoginPage";
 import { NavigationBar } from "./components/NavigationBar/NavigationBar";
 import { RegisterPage } from "./components/RegisterPage/RegisterPage";
 import { AddCardPage } from "./components/AddCardPage/AddCardPage";
+import { FavoriteCardsPage } from "./components/FavoriteCardsPage/FavoriteCardsPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element = {<RegisterPage/>} />
         <Route path="/addcard" element = {<AddCardPage/>}/>
+        <Route path="/favorite" element = {<FavoriteCardsPage/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/FavoriteCardsPage/FavoriteCardsPage.jsx
@@ -0,0 +1,5 @@
+export const FavoriteCardsPage = () => {
+  return <div className="flex">
+    
+  </div>;
+};
